2ac04733d4762148eaf9560b34b4c5b743482141
TatevikBelokon <man.tatevic@yandex.ru>
2020-09-19 21:44:25 +0300

    add little style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
+import './App.css'
 
 const App = () => {
 	const items = [
@@ -11,8 +12,8 @@ const App = () => {
 	];
 
 	return(
-		<div>
-			<h1>Заголовок</h1>
+		<div className="wrap">
+			<h1 className="wrap__title">Заголовок</h1>
 			<InputItem />
 			<ItemList items={items}/>
 			<Footer count={6} />
