3070ee7f5ed04569e948e1411d0190a44b3c571c
TatevikBelokon <man.tatevic@yandex.ru>
2020-10-12 00:12:55 +0300

    change material ui component style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const StyledButton = withStyles({  
+  label: {
+    textTransform: 'capitalize',
+        color: '#565555',
+  },
+})(Button);
 
 const Footer = ({count})=> (
 	<div className={styles.wrap}>
 		<span className={styles.wrap}>{count} items left</span>
-		<ButtonGroup aria-label="small 
-					 outlined button group" 
-					 size="small"
-					 className={styles.btnGroup}>
-			<Button>All</Button>
-		    <Button>Active</Button>
-		    <Button>Completed</Button>
-      	</ButtonGroup>	
-		<Button variant="outlined" size="small">Clear completed</Button>
+		<ButtonGroup aria-label="small outlined button group" size="small">
+			<StyledButton>All</StyledButton>
+		    <StyledButton>Active</StyledButton>
+		    <StyledButton>Completed</StyledButton>
+      	</ButtonGroup>			
+		<StyledButton variant="outlined" size="small">clear completed</StyledButton>
 	</div>
 	);
 
